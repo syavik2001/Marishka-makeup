e0f028a3d61f074faf088205d24de2541a13e29c
Vyacheslav Vinichenko <syavik2001@gmail.com>
2024-04-27 02:44:21 +0300

    next2

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 300;
+        let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
