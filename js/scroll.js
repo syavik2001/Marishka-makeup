d2bb2344d1b361f82e829686ef6744217b92f63a
Vyacheslav Vinichenko <syavik2001@gmail.com>
2024-04-28 23:56:18 +0300

    a16

--- js/scroll.js
+++ js/scroll.js
@@ -1,32 +1,43 @@
-gsap.registerPlugin(ScrollTrigger);
+//gsap.registerPlugin(ScrollTrigger);
 
 //ScrollTrigger.defaults({ smoothScrolling: false });
 
-let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
 
-itemsL.forEach(item => {
-  gsap.fromTo(item, { opacity: 0, x: -250 }, {
-    opacity: 1, x: -10,
-    scrollTrigger: {
-      trigger: item,
-      start: '-1150',
-      end: '-200',
-      scrub: true,
-    }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+  let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+  itemsL.forEach(item => {
+    gsap.fromTo(item, { opacity: 0, x: -250 }, {
+      opacity: 1, x: -10,
+      scrollTrigger: {
+        trigger: item,
+        start: '-1150',
+        end: '-200',
+        scrub: true,
+      }
+    })
   })
-})
 
-let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+  let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
 
-itemsR.forEach(item => {
-  gsap.fromTo(item, { opacity: 0, x: 250 }, {
-    opacity: 1, x: 10,
-    scrollTrigger: {
-      trigger: item,
-      start: '-1050',
-      end: '-200',
-      scrub: true,
-    }
+  itemsR.forEach(item => {
+    gsap.fromTo(item, { opacity: 0, x: 250 }, {
+      opacity: 1, x: 10,
+      scrollTrigger: {
+        trigger: item,
+        start: '-1050',
+        end: '-200',
+        scrub: true,
+      }
+    })
   })
-})
+
+});
+
+
+
+
+
 
