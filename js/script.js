4efc444fbc64d1d7aea682531cc197ae5ab6944c
Vyacheslav Vinichenko <syavik2001@gmail.com>
2024-04-28 21:14:34 +0300

    a3

--- js/script.js
+++ js/script.js
@@ -17,32 +17,32 @@ let navLinks = document.querySelectorAll('header nav a');
 //} else {
 //    header.classList.remove('sticky');
 //}
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 400;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+//window.onscroll = () => {
+//    sections.forEach(sec => {
+//        let top = window.scrollY;
+//        let offset = sec.offsetTop - 400;
+//        let height = sec.offsetHeight;
+//        let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            sec.classList.add('show-animate');
-        }
-        else {
-            sec.classList.remove('show-animate');
-        };
-    });
+//        if (top >= offset && top < offset + height) {
+//            navLinks.forEach(links => {
+//                links.classList.remove('active');
+//                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+//            });
+//            sec.classList.add('show-animate');
+//        }
+//        else {
+//            sec.classList.remove('show-animate');
+//        };
+//    });
 
     // sticky header
     //header.classList.toggle('sticky', window.scrollY > 100 || window.pageYOffset > 100);
 
     // remove toggle icon and navbar when click navbar links (scroll)
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-}
+//    menuIcon.classList.remove('bx-x');
+//    navbar.classList.remove('active');
+//}
 
 
 // animation text JS in home-block
