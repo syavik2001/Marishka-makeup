6f177b84d968622fa8cf55e5da5f9f2bdc64e646
Vyacheslav Vinichenko <syavik2001@gmail.com>
2024-04-28 19:45:13 +0300

    next7

--- js/script.js
+++ js/script.js
@@ -12,23 +12,23 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 400;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+    //sections.forEach(sec => {
+    //    let top = window.scrollY;
+    //    let offset = sec.offsetTop - 400;
+    //    let height = sec.offsetHeight;
+    //    let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            sec.classList.add('show-animate');
-        }
-        else {
-            sec.classList.remove('show-animate');
-        };
-    });
+    //    if (top >= offset && top < offset + height) {
+    //        navLinks.forEach(links => {
+    //            links.classList.remove('active');
+    //            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+    //        });
+    //        sec.classList.add('show-animate');
+    //    }
+    //    else {
+    //        sec.classList.remove('show-animate');
+    //    };
+    //});
 
     // sticky header
     let header = document.querySelector('header');
